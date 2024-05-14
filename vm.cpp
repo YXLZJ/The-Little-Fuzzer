e0c4adeb7bea16172ac047cd1a067e6c9b158b5a
kszlzj <lzij5923@uni.sydney.edu.au>
2024-05-14 16:34:44 +1000

    add benchmark

--- vm.cpp
+++ vm.cpp
@@ -77,124 +77,160 @@ public:
         this->getshortcut();
     }
 
-void compile(string file, bool show)
+    void JIT(string file, bool show)
     {
-        string code = R"(#include<stdio.h>
-#include<string.h>
-#include<time.h>
-#include<stdlib.h>
-#define next(l)\
-    seed ^= seed <<13;\
-    seed ^= seed >>17;\
-    seed ^= seed <<5;\
-    branch = seed%l
+        string code = R"(#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
+#define BUFFER_SIZE 512*1024*1024   // buffer for storing text
+)";
+        code += "#define MAX_DEPTH " + to_string(this->maxdepth) + "\n";
+        code += R"(
+//xor to get random number
+#define xor(l) \
+    seed ^= seed << 13; \
+    seed ^= seed >> 17; \
+    seed ^= seed << 5; \
+    branch = seed % l
 
-#define BUFFER_SIZE 512*1024*1024
+#define NEXT() goto *program[pc++] // fetch next instruction
 
-#define extend(c)\
-    (buffer).data[(buffer).top++] = c;
+#define initStack() do { \
+    stack.top = -1; \
+    stack.capacity = MAX_DEPTH; \
+} while (0)
 
-#define clean()\
-    buffer.top = 0
-     
-#define printbuff()\
-    for (int i = 0; i < (buffer).top; i++) { \
-        putchar(buffer.data[i]); \
-    } \
-    putchar('\n')
-    
+#define pop() (stack.top--) 
+
 typedef struct {
     char data[BUFFER_SIZE];
     unsigned top;
 } Buffer;
-Buffer buffer;
 
-unsigned branch;
-unsigned cursor;
-unsigned seed;
+Buffer buffer;  // Declare a global buffer
+
+#define extend(c) { \
+    if (buffer.top < BUFFER_SIZE) { \
+        buffer.data[buffer.top++] = c; \
+    } \
+}
+
+#define printbuff() { \
+    for (unsigned i = 0; i < buffer.top; i++) { \
+        putchar(buffer.data[i]); \
+    } \
+    putchar('\n'); \
+}
+
+#define clean() { \
+    buffer.top = 0; \
+}
+
+//frame used to record current context
+typedef struct {
+    void* Program[64]; //allow 64 options for each rule, adding hitting rate in chache.  
+    unsigned int PC;    // program counter 
+} frame;
+
+// the stack from frame
+typedef struct {
+    frame frames[MAX_DEPTH];     //the first pointer of frame
+    int top;              // the top of stack
+    int capacity;         // the capacity of stack
+} Stack;
+
+unsigned seed;  // Random seed
+unsigned branch;  // To hold branch value
+Stack stack;
+unsigned pc; //program counter;
+void* program[1000];
+
+
+int main() {
+    srand(time(NULL));
+    seed = rand();  // Initialize the seed
+    initStack();
 )";
-        code += "#define MAXDEPTH " + to_string(this->maxdepth) + "\n";
-        // creat signature
-        for (int i = 0; i < this->nodes.size(); i++)
-        {
-            code += "void func_" + to_string(reinterpret_cast<uintptr_t>(mp[nodes[i]->name])) + "(unsigned depth);\n";
-        }
-
-        string functions = "";
-        for (int i = 0; i < this->nodes.size(); i++)
-        {
-            string function = "";
-            string functionname = "void func_" + to_string(reinterpret_cast<uintptr_t>(mp[nodes[i]->name])) + "(unsigned depth){\n";
-            string body = "";
-            if (nodes[i]->tp == Type::terminal)
-            {
-                for (int j = 0; j < nodes[i]->name.size(); j++)
+        code += "    unsigned sum = 0;\n";
+        code += "    clock_t start = clock();\n";
+        code += "    unsigned int iterator = 0;\n";
+        code += "    memcpy(program, (void*[]){";
+        code += "&&func_" + to_string(reinterpret_cast<uintptr_t>(this->start)) + ",&&HALT}, sizeof(void*[2]));\n";
+        code += "START:\n";
+        code += R"(    NEXT();
+    RET:
+        pc = stack.frames[stack.top].PC;
+        memcpy(program, stack.frames[stack.top].Program, sizeof(void*[64]));
+        pop();
+        NEXT();
+    HALT:
+        stack.top = -1;
+        pc = 0;
+        goto end;
+)";
+        string blocks = "";
+        for(auto &x:this->nodes){
+            string block = "";
+            block += "    func_"+to_string(reinterpret_cast<uintptr_t>(x))+":\n";
+            if(x -> tp == Type::terminal){
+                for (int j = 0; j < x->name.size(); j++)
                 {
-                    body += "    extend(" + to_string((unsigned)nodes[i]->name[j]) + ");\n";
+                    block += "        extend(" + to_string((unsigned)x->name[j]) + ");\n";
                 }
+                block += "        NEXT();\n";
+            } else if(x -> tp == Type::non_terminal){
+                block += "        if(stack.top==MAX_DEPTH-1){\n";
+                for (int j = 0; j < this->shortcut[x].size(); j++)
+                {
+                    block += "            extend(" + to_string((unsigned)shortcut[x][j]) + ");\n";
+                }
+                block += "            NEXT();\n";
+                block += "        }\n";
+                block += "        xor(" + to_string(x->subnode.size()) + ");\n";
+                block += "        stack.top++;\n";
+                block += "        memcpy(stack.frames[stack.top].Program, program, sizeof(void*[64]));\n";
+                block += "        stack.frames[stack.top].PC = pc;\n";
+                block += "        pc = 0;\n";
+                block += "        program[0] = &&RET;\n";
+                block += "        switch (branch)\n";
+                block += "        {\n";
+                for(int j=0;j<x->subnode.size();j++){
+                    block += "        case "+to_string(j)+":\n";
+                    block += "            goto func_"+to_string(reinterpret_cast<uintptr_t>(x->subnode[j]))+";\n";
+                    block += "            break;\n";
+                }
+                block += "        }\n";
+            } else if(x->tp == Type::expression){
+                block += "        pc = 0;\n";
+                block += "        memcpy(program, (void*[]){";
+                for(auto &j:x->subnode){
+                    block+="&&func_" + to_string(reinterpret_cast<uintptr_t>(j)) + ",";
+                }
+                block += "&&RET}, sizeof(void*["+to_string(x->subnode.size()+1)+"]));\n";
+                block += "        NEXT();\n";
             }
-            else if (nodes[i]->tp == Type::non_terminal)
-            {
-                body += "    if(depth>MAXDEPTH){\n";
-                for (int j = 0; j < shortcut[nodes[i]].size(); j++)
-                {
-                    body += "        extend(" + to_string((unsigned)shortcut[nodes[i]][j]) + ");\n";
-                }
-                body += "        return;\n";
-                body += "    }\n";
-                body += "    next(" + to_string(nodes[i]->subnode.size()) + ");\n";
-                body += "    switch(branch){\n";
-                for (int j = 0; j < nodes[i]->subnode.size(); j++)
-                {
-                    body += "       case " + to_string(j) + ":\n";
-                    body += "           func_" + to_string(reinterpret_cast<uintptr_t>(nodes[i]->subnode[j])) + "(depth+1);\n           break;\n";
-                }
-                body += "    }\n";
-            }
-            else if (nodes[i]->tp == Type::expression)
-            {
-                // body += "    if(depth>MAXDEPTH){\n";
-                // for (int j = 0; j < shortcut[nodes[i]].size(); j++)
-                // {
-                //     body += "        extend(" + to_string((unsigned)shortcut[nodes[i]][j]) + ");\n";
-                // }
-                // body += "        return;\n";
-                // body += "    }\n";
-                for (int j = 0; j < nodes[i]->subnode.size(); j++)
-                {
-                    body += "    func_" + to_string(reinterpret_cast<uintptr_t>(nodes[i]->subnode[j])) + "(depth+1);\n";
-                }
-            }
-            string end = "}\n";
-            function = functionname + body + end;
-            functions += function;
+            blocks += block;
         }
-        string main_func = "int main(){\n";
-        main_func += "    srand((unsigned int)time(NULL));\n";
-        main_func += "    seed = rand();\n";
-        main_func += "    unsigned sum = 0;\n";
-        main_func += "    clock_t start = clock();\n";
-        main_func += "    for(unsigned i=0;;i++){\n";
-        main_func += "        func_" + to_string(reinterpret_cast<uintptr_t>(this->start)) + "(0);\n";
-        main_func += "        sum+=buffer.top;\n";
-        main_func += "        if ((i&0xFFFF) == 0){\n";
-        main_func += "            clock_t end = clock();\n";
-        main_func += "            double duration = (double)(end-start)/CLOCKS_PER_SEC;\n";
-        main_func += "            printf(\"Throughput Rate: \%.4f MB/s\\n\",(double)sum/duration/1024/1024);\n";
-        if (show)
-        {
-            main_func += "            printbuff();\n";
+        code += blocks;
+        code += R"(    end:
+    iterator++;
+    sum += buffer.top;
+    if ((iterator&0xFFFF) == 0){
+        clock_t end = clock();
+        double duration = (double)(end-start)/CLOCKS_PER_SEC;
+        printf("Throughput Rate: %.4f MB/s\n",(double)sum/duration/1024/1024);
+    )";
+        if(show){
+            code+="        printbuff();\n";
         }
-        main_func += "            start = end;\n";
-        main_func += "            sum = 0;\n";
-        main_func += "        }\n";
-        main_func += "        clean();\n";
-        main_func += "    }\n";
-        main_func += "    return 0;\n";
-        main_func += "}";
-        code += functions;
-        code += main_func;
+        code += "        start = end;\n";
+        code += "        sum = 0;\n";
+        code += "    }\n";
+        code += "    clean();\n";
+        code += "    goto START;\n";
+        code += "}";
         std::ofstream ofs(file, std::ofstream::out | std::ofstream::trunc);
         ofs << code;
         ofs.close();
@@ -322,10 +358,10 @@ int main(int argc, char *argv[])
     std::ifstream f(path);
     json content = json::parse(f);
     Grammar gram = Grammar(content, depth);
-    gram.compile(outputFile, show);
-    string compile = "clang " + outputFile + " -O2 " + "-o compiled_io.out";
+    gram.JIT(outputFile, show);
+    string compile = "clang " + outputFile + " -O2 " + "-o vmio.out";
     system(compile.c_str());
-    string runcode = "./compiled_io.out";
+    string runcode = "./vmio.out";
     system(runcode.c_str());
     return 0;
 }
